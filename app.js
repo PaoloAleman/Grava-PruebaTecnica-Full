451831f9d71aa9d70d42ad51ec486f79b8da9e1f
Paolo Aleman <paoloaleman86@gmail.com>
2024-12-03 00:20:41 -0300

    /api/users completed

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function initialize() {
         statusLevels: true
     }));
     app.use(cors());
+    app.use(express.json());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cookieParser());
